2522142f59e9f400c5f2c5d93bfcc92aff769a43
FabricioMenon <menon22.fm@gmail.com>
2023-06-14 15:23:12 -0300

    Atualização

--- src/viwer/JanelaPrincipal.java
+++ src/viwer/JanelaPrincipal.java
@@ -5,6 +5,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JToggleButton;
 
 public class JanelaPrincipal extends JFrame {
 
@@ -36,6 +38,26 @@ public class JanelaPrincipal extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnAgenda = new JButton("Agendamento");
+		btnAgenda.setBounds(53, 30, 117, 55);
+		contentPane.add(btnAgenda);
+		
+		JButton btnAtend = new JButton("Atendimento");
+		btnAtend.setBounds(255, 30, 117, 55);
+		contentPane.add(btnAtend);
+		
+		JButton btnNewButton_2 = new JButton("Cliente");
+		btnNewButton_2.setBounds(53, 130, 117, 48);
+		contentPane.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Barbeiro");
+		btnNewButton_3.setBounds(255, 130, 117, 48);
+		contentPane.add(btnNewButton_3);
+		
+		JButton btnNewButton = new JButton("Salvar");
+		btnNewButton.setBounds(304, 226, 117, 29);
+		contentPane.add(btnNewButton);
 	}
-
 }
