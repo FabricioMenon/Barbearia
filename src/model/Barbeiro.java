6f66c540d507f6b4c9b7b196a97c15ff27393ab8
VitorBarros-27 <84398653+VitorBarros-27@users.noreply.github.com>
2023-06-26 10:31:44 -0300

    Exclusão da classe super e correção de código em Barbeiro.java

--- src/model/Barbeiro.java
+++ src/model/Barbeiro.java
@@ -4,68 +4,59 @@ public class Barbeiro {
 	final public static int TAM_CPF = 14;
 	final public static int TAM_MIN_NOME = 6;
 	final public static int TAM_MAX_NOME = 40;
-	
-	
-	private String nomeBar;
+
+	private String cpfBarbeiro;
+	private String nomeBarbeiro;
 	/**
-	 * @param nomeBar= a
-	 * @param cpfBar
+	 * @param nomeBarbeiro= a
+	 * @param cpfBarbeiro
 	 */
-	public Barbeiro(String nomeBar, String cpfBar) {
-		super();
-		this.nomeBar = nomeBar;
-		this.cpfBar = cpfBar;
+	public Barbeiro(String nomeBarbeiro, String cpfBarbeiro) {
+		this.setNomeBarbeiro = nomeBarbeiro;
+		this.setCpfBarbeiro = cpfBarbeiro;
 	}
-	private String cpfBar;
-	
 
-	public Barbeiro() {
-		// TODO Auto-generated constructor stub
+	public String getnomeBarbeiro() {
+		return nomeBarbeiro;
 	}
 
 
-	public String getNomeBar() {
-		
-		return nomeBar;
+	public void setNomeBarbeiro(String nomeBarbeiro) throws Exception {
+		validarNome(nomeBarbeiro);
+		this.nomeBarbeiro = nomeBarbeiro;
 	}
 
 
-	public void setNomeBar(String nomeBar) throws Exception {
-		validarNome(nomeBar);
-		this.nomeBar = nomeBar;
+	public String getcpfBarbeiro() {
+		return cpfBarbeiro;
 	}
 
 
-	public String getCpfBar() {
-		return cpfBar;
-	}
-
-
-	public void setCpfBar(String cpfBar) throws Exception {
-		validarCpf(cpfBar);
-		this.cpfBar = cpfBar;
+	public void setCpfBarbeiro(String cpfBarbeiro) throws Exception {
+		validarCpf(cpfBarbeiro);
+		this.cpfBarbeiro = cpfBarbeiro;
 	}
 	
 
 
-	public static void validarNome(String nomeBar) throws Exception {
-		if(nomeBar == null)
+	public static void validarNome(String nomeBarbeiro) throws Exception {
+		if(nomeBarbeiro == null)
 			throw new Exception("O nome não pode ser nulo!");
-		if(nomeBar.length() < TAM_MIN_NOME || nomeBar.length() > TAM_MAX_NOME)
+		if(nomeBarbeiro.length() < TAM_MIN_NOME || nomeBarbeiro.length() > TAM_MAX_NOME)
 			throw new Exception("O nome deve ter entre " + TAM_MIN_NOME + " e " + TAM_MAX_NOME + " caracteres!");
-		for(int i = 0; i < nomeBar.length(); i++) {
-			char c = nomeBar.charAt(i);
+		for(int i = 0; i < nomeBarbeiro.length(); i++) {
+			char c = nomeBarbeiro.charAt(i);
 			if( ! Character.isAlphabetic(c) && !Character.isSpaceChar(c))
 				throw new Exception("O nome deve ter conter somentes letras e espaço em branco!");
 		}
 	}
-public static void validarCpf(String cpfBar) throws Exception {
-	if(cpfBar == null)
+public static void validarCpf(String cpfBarbeiro) throws Exception {
+	if(cpfBarbeiro == null)
 		throw new ModelException("O cpf não pode ser nulo!");
-	if(cpfBar.length() != TAM_CPF)
+	if(cpfBarbeiro.length() != TAM_CPF)
 		throw new Exception("O cpf deve ter " + TAM_CPF + " caracteres!");
 	for(int i = 0; i < TAM_CPF; i++) {
-		char c = cpfBar.charAt(i);
+		char c = cpfBarbeiro.charAt(i);
 		switch(i) {
 			case 3:
 			case 7: 
